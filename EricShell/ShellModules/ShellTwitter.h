e490ff7c98665ee722ac4ed1c835f317977f77bd
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2012-03-27 02:30:47 +0900

    シェルのTwitterモジュールを作るよ

---
+++ EricShell/ShellModules/ShellTwitter.h
@@ -0,0 +1,13 @@
+//
+//  ShellTwitter.h
+//  EricShell
+//
+//  Created by 川上 大樹 on 12/03/27.
+//  Copyright (c) 2012 University of Tsukuba. All rights reserved.
+//
+
+#import "ShellModule.h"
+
+@interface ShellTwitter : ShellModule
+
+@end
---
+++ EricShell/ShellModules/ShellTwitter.m
@@ -0,0 +1,13 @@
+//
+//  ShellTwitter.m
+//  EricShell
+//
+//  Created by 川上 大樹 on 12/03/27.
+//  Copyright (c) 2012 University of Tsukuba. All rights reserved.
+//
+
+#import "ShellTwitter.h"
+
+@implementation ShellTwitter
+
+@end
